edfc955f22da9b566b4aef15d8b0b5c2e7547944
Tony <tt2362@gmail.com>
2020-06-06 10:30:28 -0400

    changed left shift back to regular shift

--- keyboards/kinesis/keymaps/tony/keymap.c
+++ keyboards/kinesis/keymaps/tony/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_EQL, KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
            KC_TAB, KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
            KC_ESC, KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
-           MT(MOD_LSFT, KC_F3),KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
+           KC_LSFT, KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
                    KC_GRV ,KC_INS ,KC_LEFT,KC_RGHT,
 			   KC_LCTL,KC_LALT,
                                     LT(L1,KC_HOME),
